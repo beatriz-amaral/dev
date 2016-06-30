80d2a7f668cb1a94b1912202c58b71b17bea765c
Felipe Luiz Tortella <felipetortella@localhost.localdomain>
2016-06-30 16:32:51 -0300

    Adding base project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Savetyreapp::Application.config.secret_token = '94d52e2cee440b9755b084e616cfb421cfa7eb583f3b552cc3a76aef5761fae4a04dcfad7572b74687c75c6e59e0955be132e16543a74dab5313c09c4b3fe29f'
